fe5ada0dba82179e23eef5c7b4a3a81878ec0c4d
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-06 12:05:17 +0530

    [Ashwani]- Added repeat statement till the winning position of 100 is reached

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,24 +12,35 @@
             Console.WriteLine("Player 1 is at position : " + position);
 
             Random random= new Random();
-            int die = random.Next(1, 7);
-            Console.WriteLine("Player rolls a die and get : {0}", die);
 
-            int check = random.Next(0, 3);
+            while (position <= 100)
+            {
 
-            if (check == 0)
-            {
-                Console.WriteLine("No Play");
-            }
-            else if (check == 1)
-            {
-                Console.WriteLine("Player Moves Ahead by {0} from ladder",die);
-                position += die;
-            }
-            else
-            {
-                Console.WriteLine("Player Moves Behind by {0} from Snake", die);
-                position -= die;
+
+                int die = random.Next(1, 7);
+                Console.WriteLine("Player rolls a die and get : {0}", die);
+
+                int check = random.Next(0, 3);
+
+                if (check == 0)
+                {
+                    Console.WriteLine("No Play");
+                }
+                else if (check == 1)
+                {
+                    Console.WriteLine("Player Moves Ahead by {0} from ladder", die);
+                    position += die;
+                }
+                else
+                {
+                    Console.WriteLine("Player Moves Behind by {0} from Snake", die);
+                    position -= die;
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                Console.WriteLine("Player current position is : "+position);
             }
             Console.WriteLine("Current Position is : {0}", position);
         }
