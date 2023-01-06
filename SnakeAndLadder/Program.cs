be1a32247d393d6f819cfd30f96978723bbcf631
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-06 11:54:10 +0530

    [Ashwani]- Added condition for ladder, snake or no play

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -15,6 +15,23 @@
             int die = random.Next(1, 7);
             Console.WriteLine("Player rolls a die and get : {0}", die);
 
+            int check = random.Next(0, 3);
+
+            if (check == 0)
+            {
+                Console.WriteLine("No Play");
+            }
+            else if (check == 1)
+            {
+                Console.WriteLine("Player Moves Ahead by {0} from ladder",die);
+                position += die;
+            }
+            else
+            {
+                Console.WriteLine("Player Moves Behind by {0} from Snake", die);
+                position -= die;
+            }
+            Console.WriteLine("Current Position is : {0}", position);
         }
     }
 }
