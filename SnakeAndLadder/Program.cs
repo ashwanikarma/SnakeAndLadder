0d194ba33ff5ef42ab63985b6717ec6206176d48
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-06 12:18:05 +0530

    [Ashwani] - Added condition that winning will be at 100 only

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,11 +11,11 @@
             Console.WriteLine("Welcome Player 1");
             Console.WriteLine("Player 1 is at position : " + position);
 
-            Random random= new Random();
+            Random random = new Random();
 
             while (position <= 100)
             {
-
+                int temp = position;
 
                 int die = random.Next(1, 7);
                 Console.WriteLine("Player rolls a die and get : {0}", die);
@@ -40,6 +40,10 @@
                 {
                     position = 0;
                 }
+                else if (position > 100)
+                {
+                    position = temp;
+                } 
                 Console.WriteLine("Player current position is : "+position);
             }
             Console.WriteLine("Current Position is : {0}", position);
