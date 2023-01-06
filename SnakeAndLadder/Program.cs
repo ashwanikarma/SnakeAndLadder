c316e85f04e1986e62e8762b24c92dcc762ec977
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-06 11:05:09 +0530

    [Ashwani] - Player starting position 0 added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Snake & Ladder Simulator");
+
+            int position = 0;
+
+            Console.WriteLine("Welcome Player 1");
+            Console.WriteLine("Player 1 is at position : " + position);
+
         }
     }
 }
