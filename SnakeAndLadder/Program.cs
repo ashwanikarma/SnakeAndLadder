9378505a7a62a1fe7dea9cbefc1e378e9f4b729d
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-06 10:59:59 +0530

    [Ashwani]- Welcome Message added

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake & Ladder Simulator");
+        }
+    }
+}
